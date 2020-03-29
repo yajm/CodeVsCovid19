4c00de770145bf330bbda13f7becdf172446fb84
Yannick Mueller <muelleya@student.ethz.ch>
2020-03-29 13:43:12 +0200

    Rounded Cards

--- public_html/game/assets/js/player.js
+++ public_html/game/assets/js/player.js
@@ -380,12 +380,30 @@ class Card{
 
 
   draw(x,y){
-    this.x=x
-    this.y=y
+    this.x=x;
+    this.y=y;
     var c = document.getElementById("gameField");
     var ctx = c.getContext("2d");
-    ctx.drawImage(this.image,x,y,this.width, this.height)
+    ctx.save();
+    roundedImage(x,y,this.width, this.height, 10);
+    ctx.clip();
+    ctx.drawImage(this.image,x,y,this.width, this.height);
+    ctx.restore();
   }
+
+  function roundedImage(x,y,width,height,radius){
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+    }
 
   isClicked(x,y) {
     return this.x <= x && this.x + this.width >= x &&  this.y <= y && this.y + this.height >= y
